87a84ad9675ddc1d8215b830c74d849fceaf1c8c
Sumit <sumitadhikari2341@gmail.com>
2025-10-24 17:55:13 +0900

    tips category added

--- components/categories.tsx
+++ components/categories.tsx
@@ -17,6 +17,14 @@ const categories = [
     color: "text-pink-600 dark:text-pink-400",
   },
   {
+    icon: Brain,
+    title: "Tips",
+    count: "100+ Videos",
+    color: "text-purple-600 dark:text-purple-400",
+  },
+
+
+  {
     icon: TrendingUp,
     title: "Business",
     count: "120+ courses",
