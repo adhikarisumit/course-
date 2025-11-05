8d3aae8007993ec8b6211291363d2229bd137552
Sumit <sumitadhikari2341@gmail.com>
2025-11-05 12:25:16 +0900

    spell correction office

--- app/softwares/page.tsx
+++ app/softwares/page.tsx
@@ -14,7 +14,7 @@ const softwareLinks = [
     category: "Browser",
   },
   {
-    name: "MIcrosoft Ofice (Windows)",
+    name: "MIcrosoft Office (Windows)",
     description: "Microsoft office products ",
     url: "https://www.microsoft.com/en-us/microsoft-365/download-office?msockid=0cbd0bfcc18a61733d491da9c09460ec#download",
     category: "Software",
