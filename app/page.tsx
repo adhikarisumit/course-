f6bb7d2fa54d591d69380d22cf0125901f524f23
Sumit <sumitadhikari2341@gmail.com>
2025-12-09 17:06:45 +0900

    website speed improvement

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import { useState } from "react"
+import dynamic from 'next/dynamic'
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import { Categories } from "@/components/categories"
-import { FeaturedCourses } from "@/components/featured-courses"
-import { Resources } from "@/components/resources"
-import { Footer } from "@/components/footer"
+
+const FeaturedCourses = dynamic(() => import('@/components/featured-courses').then(mod => ({ default: mod.FeaturedCourses })), { ssr: true })
+const Resources = dynamic(() => import('@/components/resources').then(mod => ({ default: mod.Resources })), { ssr: true })
+const Footer = dynamic(() => import('@/components/footer').then(mod => ({ default: mod.Footer })), { ssr: true })
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState("")
